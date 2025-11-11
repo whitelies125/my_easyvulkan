be3b460fa84a27fc3ec4a201ac62bb8ca7970283
whitelies125 <lzb125125@gmail.com>
2025-11-11 21:59:37 +0800

    init

---
+++ src/VKBase.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "EasyVKStart.h"
+// 定义vulkan命名空间，之后会把Vulkan中一些基本对象的封装写在其中
+namespace vulkan {}
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "GlfwGeneral.hpp"
+
+int main()
+{
+    if (!InitializeWindow({1280, 720})) return -1;  // 来个你讨厌的返回值
+    while (!glfwWindowShouldClose(pWindow)) {
+        /*渲染过程，待填充*/
+
+        glfwPollEvents();
+        TitleFps();
+    }
+    TerminateWindow();
+    return 0;
+}
